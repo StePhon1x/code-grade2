9d09ad6b1e93338d0d35d7273502835d2ee2e313
zjy <1786974219@qq.com>
2025-09-01 23:11:51 +0800

    存放0822代码

---
+++ 0822/0822.c
@@ -0,0 +1,68 @@
+// 0822 再分肥宅水.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+//#include <iostream>
+//#include <iomanip>
+//using namespace std;
+//
+//int main()
+//{
+//    double t;
+//    int n;
+//    cin >> t >> n;
+//    cout << fixed << setprecision(3) << t/n << endl << n*2; 
+//    return 0;
+//////}
+//0822 .cpp
+//#include <iostream>
+//#include <iomanip>
+//#include <cmath>
+//using namespace std;
+//int main()
+//{
+//	double a, b, c, p,s;
+//	cin >>fixed >> setprecision(2) >> a >> b >> c;
+//	p = (a + b + c) / 2;
+//	s = sqrt(p * (p - a) * (p - b) * (p - c));
+//	cout << fixed << setprecision(1) << s << endl;
+//	return 0;
+//}
+//0822 .cpp
+//#include <iostream>
+//#include <cmath>
+//using namespace std;
+//int main()
+//{
+//	double s, v;
+//	int n, t, a, b;
+//	cin >> s >> v;
+//	n = 8 * 60 + 24 * 60;
+//	t = ceil(s / v) + 10;
+//	n -= t;
+//	if (n > 24 * 60) {
+//		n -= 24 * 60;
+//	}
+//	a = n / 60;
+//	b = n % 60;
+//	printf("%02d:%02d", a, b);
+//	return 0;
+//}
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	int h, r, a;
+//	cin >> h >> r;
+//	a = 20000 / (3.14 * r * r * h) + 1;
+//	cout << a;
+//	return 0;
+//}
+
+
+
+
+
+
+
+
+
