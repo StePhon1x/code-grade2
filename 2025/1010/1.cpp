f76fff3fc07159cd18ad71ea4225dfea902ae703
StePhon1x <1786974219@qq.com>
2025-10-10 18:47:17 +0800

    add 1.cpp的题目描述

--- 2025/1010/1.cpp
+++ 2025/1010/1.cpp
@@ -4,10 +4,11 @@ using namespace std;
 int reverseNumber(int n)
 {
     int rev = 0;
-    while(n!=0){
-    int digit = n % 10;
-    rev = rev * 10 + digit;
-    n = n/10;
+    while (n != 0)
+    {
+        int digit = n % 10;
+        rev = rev * 10 + digit;
+        n = n / 10;
     }
     return rev;
 }
@@ -21,3 +22,12 @@ int main()
 
     return 0;
 }
+
+// 题目描述
+// 给定一个整数 N，请将该数各个位上数字反转得到一个新数。新数也应满足整数的常见形式，即除非给定的原数为零，否则反转后得到的新数的最高位数字不应为零（参见样例 2）。
+
+// 输入格式
+// 一个整数 N。
+
+// 输出格式
+// 一个整数，表示反转后的新数。
