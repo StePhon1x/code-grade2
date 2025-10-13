a50f499fbff7282d0bc92597c6c8e37c5265c429
StePhon1x <1786974219@qq.com>
2025-10-13 17:51:01 +0800

    add 1013 code

---
+++ 2025/1013/tempCodeRunnerFile.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    long long a=1, b=1, c;
+    for(int i=3; i<=n; i++){
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    cout << c << ".00" << endl;
+
+    return 0;
+}
